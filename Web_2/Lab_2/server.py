18bd038e930e9df09583b86f87e3af4222a85cad
SofiikaKh <61017878+SofiikaKh@users.noreply.github.com>
2020-10-16 10:28:03 +0300

    lab2

--- Web_2/Lab_2/server.py
+++ Web_2/Lab_2/server.py
@@ -3,7 +3,7 @@ import threading
 import time
 from threading import Timer
 import datetime
-import xml
+import xml.etree.ElementTree as ET
 
 host = 'localhost'
 port = 777
@@ -17,24 +17,29 @@ listClients = []
 listconnections =[]
 i=0
 startTimer=0
-def printing(list1):
-    for i in range(len(listClients)):
-        print(listClients[i])
 
 def timeout():
     now = datetime.datetime.now()
     print ("timestartat:  ",now.strftime("%d-%m-%Y %H:%M"))
-    printing(listClients)
+    sending(listClients)
 
 t = Timer(10, timeout)
 def sending(listClients):
+    root = ET.Element("LIST")  # рутовый элемент
     for i in range (len(listClients)):
-        ur_sk = listconnections[i]
-        x = XML.dumps(listClients) 
-        ur_sk.send(x.encode("utf-8"))
-        i =i+1
-
-while True: 
+        query = ET.SubElement(root, "time")  # добавляем дочерний элемент к root
+        query.text = str(listClients[i][0])  # добавляем значение элемента
+        query = ET.SubElement(root, "id")  # добавляем дочерний элемент к root
+        query.text = str(listClients[i][1])  # добавляем значение элемента
+        query = ET.SubElement(root, "surname")  # добавляем дочерний элемент к root
+        query.text = str(listClients[i][2])  # добавляем значение элемента
+        message = ET.tostring(root, "utf-8")
+        doc = '<?xml version="1.0" encoding="UTF-8"?>' + message.decode("utf-8")
+        print (doc)
+    for i in range (len(listconnections)):
+        ur_sk=listconnections[i]
+        ur_sk.send(doc.encode("utf-8"))
+while i<16: 
     print('wait connection...')
     conn, addr = tcp_socket.accept()
     conn.setblocking(False)
@@ -51,8 +56,6 @@ while True:
         listClients.append([tnow.strftime("%d-%m-%Y %H:%M"),i,data])    
  
 else: 
-    for conn in listconnections:
-        sending(listClients)
     for conn in listconnections:
         conn.close
     print("The end")
